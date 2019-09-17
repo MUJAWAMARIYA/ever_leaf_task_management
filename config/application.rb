3263143dd17029dc9927b1aca0a51bc1488edb38
MUJAWAMARIYA <mumarg2015@gmail.com>
2019-09-17 09:32:56 +0200

    deploying on heroku my app of ever leaf task management

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ Time.use_zone('Tokyo') { Time.zone.now }
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.i18n.default_locale = :ja
+    config.i18n.default_locale = :en
 
     config.generators do |g|
       g.test_framework :rspec,
