cc1eb007d18b09395aa96b99d786a1606606baa7
MUJAWAMARIYA <mumarg2015@gmail.com>
2019-09-25 15:57:53 +0200

    to test step 12

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
-    validates :name, :content, :kind_of_task,  :priority,:status, presence: true    
+    validates :name, :content, presence: true    
 paginates_per 2
-# belongs_to :user
+belongs_to :user
 
 end
