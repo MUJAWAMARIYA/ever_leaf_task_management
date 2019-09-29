a39362761861bcea8972a3cac0fe3df24458eaaa
MUJAWAMARIYA <mumarg2015@gmail.com>
2019-09-29 10:34:55 +0200

    error correction on pending migration and step 16,17,18 and step

--- db/migrate/20190925070602_change_column_null_tasks.rb
+++
@@ -1,9 +0,0 @@
-class ChangeColumnNullTasks < ActiveRecord::Migration[5.2]
-  def change
-    changeColumn :tasks, :titles, :string, null: false
-    changeColumn :tasks, :content, :string, null: false
-    changeColumn :tasks, :priority, :string, null: false
-    changeColumn :tasks, :status, :string, null: false
-    
-  end
-end
--- db/migrate/20190929080925_change_column_name.rb
+++ db/migrate/20190929080925_change_column_name.rb
@@ -1,4 +1,4 @@
-class RenameTaskNameToTitle < ActiveRecord::Migration[5.2]
+class ChangeColumnName < ActiveRecord::Migration[5.2]
   def change
     rename_column :tasks, :name, :titles
   end
