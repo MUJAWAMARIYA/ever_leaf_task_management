a74ea056d40c3857b9399bbe70d2d2c3c9802f9e
priscilleumurerwa <priscille.umurerwa@gmail.com>
2019-10-21 20:26:06 +0000

    step 21 admin page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,8 @@ class UsersController < ApplicationController
   end
   def new
     @user = User.new
-  
   end
   def create
-   
     @user = User.new(user_params)
     if @user.save
       redirect_to root_path(@user.id)
