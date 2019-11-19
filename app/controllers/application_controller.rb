43e91848f1950a1e8029baa218271c975658d0eb
MUJAWAMARIYA <>
2019-11-19 12:04:05 +0000

    hr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base 
   protect_from_forgery with: :exception
   add_flash_types :danger, :info, :warning, :success
-  
   include SessionsHelper
     def self.search(term, page)
         if term
