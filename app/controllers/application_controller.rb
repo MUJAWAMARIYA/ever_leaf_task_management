c8b2e6893797df503362dad8d8d1b6a6f6506f13
priscilleumurerwa <>
2019-11-19 11:58:14 +0000

    ever leaf app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base 
   protect_from_forgery with: :exception
   add_flash_types :danger, :info, :warning, :success
+  
   include SessionsHelper
     def self.search(term, page)
         if term
